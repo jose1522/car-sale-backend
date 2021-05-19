9383d34d7ca6af4cd79e6d356ee67a437e838691
Jose Andres Moya Porras <jose.91cr@gmail.com>
2021-05-19 17:51:07 -0600

    New route update

--- app/api/routes.py
+++ app/api/routes.py
@@ -9,7 +9,7 @@ apiRouter = APIRouter()
 
 @apiRouter.get('/')
 async def index():
-    return {"hello" : "world"}
+    return {"hello" : "world updated"}
 
 
 # @apiRouter.get('/test', dependencies=[Depends(user_login)])
