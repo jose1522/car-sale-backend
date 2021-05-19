5ff07f63fdcc41960160fe53bb0b90e696cf80b2
Jose Andres Moya Porras <jose.91cr@gmail.com>
2021-05-19 12:49:15 -0600

    Change to route

--- app/api/routes.py
+++ app/api/routes.py
@@ -9,7 +9,7 @@ apiRouter = APIRouter()
 
 @apiRouter.get('/')
 async def index():
-    return {"hello" : "world 2"}
+    return {"hello" : "world"}
 
 
 # @apiRouter.get('/test', dependencies=[Depends(user_login)])
